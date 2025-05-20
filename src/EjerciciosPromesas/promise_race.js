ab4dc5b1cd521bb028223c9b730c7fab00690666
Cristopher Bueno <buenocris02@gmail.com>
2025-05-20 08:55:29 -0500

    Se resolvió conflicto y se mantuvo la versión funcional de generación de archivos

--- src/EjerciciosPromesas/promise_race.js
+++ src/EjerciciosPromesas/promise_race.js
@@ -1,32 +1,13 @@
-<<<<<<< HEAD
-const promesaRapida = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve("Promesa rápida (2 segundos) resuelta");
-=======
 //como lo solicito aqui se crearon 2 promesas con diferentes tiempos de resolucion, asi que usaremos promise_rice para saber cual se resuelve primero.
 
 const promesaRapida = new Promise((resolve) => {
     setTimeout(() => {
         resolve("wow... Promesa rapida resuelta en 2 segundos");
->>>>>>> rama-cris
     }, 2000);
 });
 
 const promesaLenta = new Promise((resolve) => {
     setTimeout(() => {
-<<<<<<< HEAD
-        resolve("Promesa lenta (5 segundos) resuelta");
-    }, 5000);
-});
-
-Promise.race([promesaRapida, promesaLenta])
-    .then((resultado) => {
-        console.log("Resultado de Promise.race:", resultado);
-    })
-    .catch((error) => {
-        console.error("Error en Promise.race:", error);
-    });
-=======
         resolve("que mal... Promesa lenta resuelta en 5 segundos");
     }, 5000);
 });
@@ -39,4 +20,3 @@ Promise.race([promesaRapida, promesaLenta])
     .catch((error) => {
         console.error("❌ Error en la carrera de promesas:", error);
     });
->>>>>>> rama-cris
