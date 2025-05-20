ccc215a455113facb650dd98a6d82b82709f6420
Cristopher Bueno <buenocris02@gmail.com>
2025-05-20 09:00:55 -0500

    se verifican errores y se explica codigo quedando funcional

--- src/EjerciciosPromesas/encadenamiento_promesas.js
+++ src/EjerciciosPromesas/encadenamiento_promesas.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-function paso1() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Paso 1 completado");
-            resolve();
-=======
 //con este codigo simularemos un flujo de pasos donde cada paso tarda 1 segundo en completarse... asi que usaremos 3 promesas y las encadenaremos con .then para que se ejecuten en orden.
 
 function paso1() {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve("✅ Paso 1 completado");
->>>>>>> rama-cris
         }, 1000);
     });
 }
@@ -19,12 +11,8 @@ function paso1() {
 function paso2() {
     return new Promise((resolve) => {
         setTimeout(() => {
-<<<<<<< HEAD
-            console.log("Paso 2 completado");
-            resolve();
-=======
+
             resolve("✅ Paso 2 completado");
->>>>>>> rama-cris
         }, 1000);
     });
 }
@@ -32,24 +20,11 @@ function paso2() {
 function paso3() {
     return new Promise((resolve) => {
         setTimeout(() => {
-<<<<<<< HEAD
-            console.log("Paso 3 completado");
-            resolve();
-=======
             resolve("✅ Paso 3 completado");
->>>>>>> rama-cris
         }, 1000);
     });
 }
 
-<<<<<<< HEAD
-// Encadenamiento de promesas
-paso1()
-    .then(() => paso2())
-    .then(() => paso3())
-    .then(() => {
-        console.log("Todos los pasos completados");
-=======
 //aqui las encadenamos
 paso1()
     .then((resultado1) => {
@@ -66,5 +41,4 @@ paso1()
     })
     .catch((error) => {
         console.error("❌ Error en el proceso:", error);
->>>>>>> rama-cris
     });
